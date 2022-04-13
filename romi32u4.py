e78f8cc1b4443cd61e52b28a7ac35862e7261647
Ron Smith <ron.smith@thataintworking.com>
2022-04-13 17:43:27 -0400

    Added FIFO properties and IMUData

--- romi32u4.py
+++ romi32u4.py
@@ -2,6 +2,7 @@
 import smbus2
 import struct
 import time
+from dataclasses import dataclass
 from lsm6 import *
 
 ENCODERS_REG = 0x27
@@ -16,6 +17,19 @@ AVR = 0x14
 IMU = 0x6b
 
 
+@dataclass
+class IMUData:
+    gy_x: int
+    gy_y: int
+    gy_z: int
+    xl_x: int
+    xl_y: int
+    xl_z: int
+
+    def __init__(self, tup):
+        self.gy_x, self.gy_y, self.gy_z, self.xl_x, self.xl_y, self.xl_z = tup
+
+
 class Romi324U:
 
     def __init__(self, bus=None, xl_odr=ODR_OFF, xl_hp=False, gy_odr=ODR_OFF, gy_hp=False):
@@ -27,10 +41,10 @@ class Romi324U:
         :param gy_hp: boolean
         """
         self.bus = bus or smbus2.SMBus(1)
-        # self.xl_odr = xl_odr
-        # self.xl_hp = xl_hp
-        # self.gy_odr = gy_odr
-        # self.gy_hp = gy_hp
+        self.xl_odr = xl_odr
+        self.xl_hp = xl_hp
+        self.gy_odr = gy_odr
+        self.gy_hp = gy_hp
 
     def _read_unpack(self, address, register, size, format):
         """
@@ -133,4 +147,27 @@ class Romi324U:
     @gy_hp.setter
     def gy_hp(self, value):
         self._write_byte_with_mask(IMU, CTRL7_G, (1 if value else 0)<<7, GY_HM_MODE_MASK)
+
+    @property
+    def fifo_odr(self):
+        # the ODR value and mask have to be shifted by one bit for FIFO
+        return self._read_byte_with_mask(IMU, FIFO_CTRL5, ODR_MASK>>1) << 1
+
+    @fifo_odr.setter
+    def fifo_odr(self, value):
+        # the ODR value and mask have to be shifted by one bit for FIFO
+        self._write_byte_with_mask(IMU, FIFO_CTRL5, value>>1, ODR_MASK>>1)
+
+    @property
+    def fifo_mode(self):
+        return self._read_byte_with_mask(IMU, FIFO_CTRL5, FIFO_MODE_MASK) << 1
+
+    @fifo_mode.setter
+    def fifo_mode(self, value):
+        self._write_byte_with_mask(IMU, FIFO_CTRL5, value, FIFO_MODE_MASK)
+
+    @property
+    def imu_data(self):
+        t = self._read_unpack(IMU, OUTX_L_G, 12, "HHHHHH")
+        return IMUData(t)
 
