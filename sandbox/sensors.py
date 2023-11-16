605bfe576517c670ba79fbddfc55870bd04a9429
Ron Smith <ron.smith@thataintworking.com>
2023-11-16 12:41:25 -0500

    Turn yellow LED on/off to correspond to lsm enable/disable

--- sandbox/sensors.py
+++ sandbox/sensors.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '..')
 
 from smbus2 import SMBus
-from romi32u4 import Romi32U4, A
+from romi32u4 import Romi32U4, A, LED_ON, LED_OFF
 from lsm6 import LSM6
 from time import sleep
 
@@ -15,12 +15,14 @@ lsm = LSM6(bus)
 def main():
     try:
         lsm.enable()
+        romi.led_yellow(LED_ON)
         while not romi.buttons[A]:
             print('GY', lsm.read_gyro(), "XL", lsm.read_accel())
             sleep(1)
 
     finally:
         lsm.disable()
+        romi.led_yellow(LED_OFF)
         romi.motors(0, 0)
 
 
