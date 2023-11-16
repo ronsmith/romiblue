b5b55550d657c4c995c6b0f0aa11e57857802972
Ron Smith <ron.smith@thataintworking.com>
2023-11-16 15:35:31 -0500

    Init LSM6 with Romi instance

--- sandbox/spin_sensors.py
+++ sandbox/spin_sensors.py
@@ -9,13 +9,13 @@ from time import sleep
 
 bus = SMBus(1)
 romi = Romi32U4(bus)
-lsm = LSM6(bus)
+lsm = LSM6(romi)
 
 
 def main():
     try:
         lsm.enable()
-        romi.motors(25, -25)
+        romi.motors(30, -30)
         while not romi.buttons.a:
             print('GY', lsm.read_gyro(), 'XL', lsm.read_accel())
             sleep(1)
