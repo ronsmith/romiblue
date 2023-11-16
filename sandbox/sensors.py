0c77e4205089c3a463e432c80915d91ce64befe5
Ron Smith <ron.smith@thataintworking.com>
2023-11-16 11:58:43 -0500

    Switched to using Pololu's lsm6.py

--- sandbox/sensors.py
+++ sandbox/sensors.py
@@ -3,23 +3,23 @@ sys.path.insert(0, '..')
 
 from smbus2 import SMBus
 from romi32u4 import Romi32U4, A
-from lsm6 import LSM6, IMU_ADRS1, ODR_166KHZ
+from lsm6 import LSM6
 from time import sleep
-from math import ceil
 
 
 bus = SMBus(1)
 romi = Romi32U4(bus)
-lsm = LSM6(bus, IMU_ADRS1, ODR_166KHZ, False, ODR_166KHZ, False)
+lsm = LSM6(bus)
+
 
 def main():
     try:
         while not romi.buttons[A]:
-            print(lsm.imu_data)
+            print('GY:', lsm.read_gyro(), "XL:", lsm.read_accel())
             sleep(1)
 
     finally:
-        romi.motors(0,0)
+        romi.motors(0, 0)
 
 
 if __name__ == "__main__":
